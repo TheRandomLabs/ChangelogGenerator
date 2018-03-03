98124768e2854f327da61546119050e5abd85b68
TheRandomLabs <therandomlabsinc@gmail.com>
2018-03-04 01:25:53 +1100

    Fix version number

--- src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
+++ src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
@@ -13,7 +13,7 @@ import com.therandomlabs.utils.io.NIOUtils;
 import com.therandomlabs.utils.misc.StringUtils;
 
 public final class ChangelogGenerator {
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.4";
 
 	private static final String NEWLINE = IOConstants.LINE_SEPARATOR;
 

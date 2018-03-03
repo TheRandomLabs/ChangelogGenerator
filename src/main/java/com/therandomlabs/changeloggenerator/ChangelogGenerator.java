931eec07d3522c28e1e6d826f86097d45506db3d
TheRandomLabs <therandomlabsinc@gmail.com>
2018-03-04 01:03:04 +1100

    Add version

--- src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
+++ src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
@@ -13,6 +13,8 @@ import com.therandomlabs.utils.io.NIOUtils;
 import com.therandomlabs.utils.misc.StringUtils;
 
 public final class ChangelogGenerator {
+	public static final String VERSION = "1.5";
+
 	private static final String NEWLINE = IOConstants.LINE_SEPARATOR;
 
 	public static void main(String[] args) throws Exception {
@@ -76,7 +78,7 @@ public final class ChangelogGenerator {
 		}
 
 		string.append("* Generated using https://github.com/TheRandomLabs/ChangelogGenerator").
-				append(NEWLINE);
+				append(" (").append(VERSION).append(")").append(NEWLINE);
 
 		final String toString = string.toString();
 		if(toString.endsWith(NEWLINE + NEWLINE)) {

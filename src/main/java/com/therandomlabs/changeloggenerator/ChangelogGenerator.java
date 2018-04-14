4fe04b1ace1cf64b0024f07f620b171faeeefc57
TheRandomLabs <therandomlabsinc@gmail.com>
2018-04-14 18:54:18 +1000

    Update version

--- src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
+++ src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
@@ -13,7 +13,7 @@ import com.therandomlabs.utils.io.NIOUtils;
 import com.therandomlabs.utils.misc.StringUtils;
 
 public final class ChangelogGenerator {
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.6";
 
 	private static final String NEWLINE = IOConstants.LINE_SEPARATOR;
 

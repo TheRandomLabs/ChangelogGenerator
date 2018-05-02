f6bea3bbe2b7aa00a836affba111d0fec2c82389
TheRandomLabs <therandomlabsinc@gmail.com>
2018-05-02 17:26:31 +1000

    Minor formatting change

--- src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
+++ src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
@@ -14,7 +14,7 @@ import com.therandomlabs.utils.io.NetUtils;
 import com.therandomlabs.utils.misc.StringUtils;
 
 public final class ChangelogGenerator {
-	public static final String VERSION = "1.7";
+	public static final String VERSION = "1.6.4";
 
 	private static final String NEWLINE = IOConstants.LINE_SEPARATOR;
 
@@ -40,8 +40,8 @@ public final class ChangelogGenerator {
 		NIOUtils.write(Paths.get("shortchangelog.txt"), getChangelog(results, true));
 	}
 
-	public static String getChangelog(ManifestComparer.Results results,
-			boolean urls) throws CurseException, IOException {
+	public static String getChangelog(ManifestComparer.Results results, boolean urls)
+			throws CurseException, IOException {
 		final StringBuilder string = new StringBuilder();
 
 		string.append(results.getOldManifest().name).append(' ').

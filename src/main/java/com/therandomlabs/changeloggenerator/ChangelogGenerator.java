a4eab473af132740cf592ff4608adc1f01c68be5
TheRandomLabs <therandomlabsinc@gmail.com>
2018-05-05 12:24:48 +1000

    Bump version

--- src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
+++ src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
@@ -14,7 +14,7 @@ import com.therandomlabs.utils.io.NetUtils;
 import com.therandomlabs.utils.misc.StringUtils;
 
 public final class ChangelogGenerator {
-	public static final String VERSION = "1.6.4";
+	public static final String VERSION = "1.6.5";
 
 	private static final String NEWLINE = IOConstants.LINE_SEPARATOR;
 

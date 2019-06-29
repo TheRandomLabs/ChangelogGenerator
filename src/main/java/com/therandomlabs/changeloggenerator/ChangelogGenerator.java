df5e7f3caceb8efe332cc3e495c823f458fb7c76
TheRandomLabs <therandomlabsinc@gmail.com>
2019-06-29 20:37:53 +1000

    Closes #18

--- src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
+++ src/main/java/com/therandomlabs/changeloggenerator/ChangelogGenerator.java
@@ -35,7 +35,7 @@ import com.therandomlabs.utils.misc.ThreadUtils;
 import static com.therandomlabs.utils.logging.Logging.getLogger;
 
 public final class ChangelogGenerator {
-	public static final String VERSION = "1.14";
+	public static final String VERSION = "1.15";
 
 	public static final ImmutableList<ModSpecificChangelogHandler> HANDLERS = new ImmutableList<>(
 			ActuallyAdditionsHandler.INSTANCE,
